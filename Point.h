5596c6a1f239e8a6cbecd2801eab4c8a3936e6c7
ryandchandraa <ryandchandra@s,itb,ac,id>
2018-04-12 21:45:20 +0700

    Tambah fungsi

--- Point.h
+++ Point.h
@@ -1,37 +1,52 @@
-Class Point {
+class Point{
 	public :
-		int GetAbsis() {
+		Point (int a, int b){
+			x = a;
+			y = b;
+		}
+	
+		int GetAbsis(){
 			return x;
 		}
-		void SetAbsis(int x) {
-			this.x = x;
+		
+		int GetOrdinat(){
+			return y;
 		}
-		void Move (int dx, int dy) {
-			x = x + dx;
-			y = y + dy;
+		
+		void SetAbsis(int a){
+			x = a;
 		}
-		int Kuadran() {
-			if (x>0) && (y>0) {
+		
+		void SetOrdinat(int b){
+			y = b;
+		}
+		
+		void Move(int dx, int dy){
+			x = x+dx;
+			y = y+dy;
+		}
+		
+		int Kuadran(){
+			if ((x>0)&&(y>0)){
 				return 1;
-			}
-			else if (x<0) && (y>0) {
+			} else if ((x<0)&&(y>0)){
 				return 2;
-			}
-			else if (x<0) && (y<0) {
+			} else if ((x<0)&&(y<0)){
 				return 3;
-			}
-			else if (x>0) && (y<0) {
+			} else if ((x>0)&&(y<0)){
 				return 4;
+			} else {
+				return 0;
 			}
 		}
-	
-		Point HasilPindah (int dx, int dy) {
+		
+		Point HasilPindah(int dx, int dy){
 			Point P(x,y);
-			P.Move (dx, dy);
+			P.Move(dx, dy);
 			return P;
 		}
-	
-	private :
+		
+	private:
 		int x;
 		int y;
-}
+};
